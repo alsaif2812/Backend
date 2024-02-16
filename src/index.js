79186e47a9370f17b96b6e11ea0869bf218cf83c
Mohammad Al Saif <alsaifsiddiqui1@gmail.com>
2024-02-16 19:02:54 +0530

    qq

--- src/index.js
+++ src/index.js
@@ -1,42 +1,16 @@
 // require('dotenv').config({path:'./env'})
 import dotenv from 'dotenv';
 import connect from './db/index.js';
+import express from 'express'
+const app = express();
 
 dotenv.config({
           path: './env'
 });
-
-
-connect();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*;(async () => {
-          try {
-                    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-                    app.on("error", (err) => {
-                              console.log(err)
-                              throw err;
-                    });
-                    app.listen(process.env.PORT,()=> {
-                              console.log(`App is listening on port ${process.env.PORT}`)
-                    })
-          } catch (error) {
-                    console.log("ERROR: " + error);
-                    throw error;
-          }
-} )()*/
+connect().then(()=> {
+          app.listen(process.env.PORT || 8000,()=> {
+                    console.log(`Server is running at port : ${process.env.PORT}`)
+          })
+}).catch((error) => {
+          console.log(error)
+})
